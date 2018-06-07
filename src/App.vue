30c46d9464b10a86a73e2f47f6b9e0e1df91c6a4
ZHU\Jun <https://git.oschina.net/whutzyl/hh.git>
2018-06-07 21:18:49 +0800

    'add'

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+          <div class="swiper-slide">Slide 1</div>
+          <div class="swiper-slide">Slide 2</div>
+          <div class="swiper-slide">Slide 3</div>
+      </div>
+      <!-- 如果需要分页器 -->
+      <div class="swiper-pagination"></div>
+      
+      <!-- 如果需要导航按钮 -->
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
+      
+      <!-- 如果需要滚动条 -->
+      <div class="swiper-scrollbar"></div>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Swiper from 'swiper';
 export default {
-  name: 'App'
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted(){
+     new Swiper ('.swiper-container', {
+    loop: true,
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+    // 如果需要前进后退按钮
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    // 如果需要滚动条
+    scrollbar: '.swiper-scrollbar',
+  })        
+  }
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,4 +54,9 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.swiper-container {
+        width: 500px;
+        height: 300px;
+        margin: 20px auto;
+    }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -95,7 +95,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="less" scoped>
 h1, h2 {
   font-weight: normal;
 }
@@ -108,6 +108,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: red;
 }
 </style>
